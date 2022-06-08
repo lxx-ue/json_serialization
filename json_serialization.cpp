07185e7048515e8f13b666f6c96b6153ed8df61e
lxx-ue <71316369+lxx-ue@users.noreply.github.com>
2022-06-09 00:02:55 +0300

    json library

--- json_serialization.cpp
+++ json_serialization.cpp
@@ -1,6 +1,24 @@
 ﻿#include <iostream>
+#include <string>
+#include <nlohmann/json.hpp>
+
+struct Person
+{
+    std::string name;
+    int age;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Person p{ "LX", 20 };
+    nlohmann::json j{};
+    j["name"] = p.name;
+    j["age"] = p.age;
+    std::cout << j << std::endl;
+
+    std::string serilizedString = j.dump();
+    Person p1{};
+    nlohmann::json j1 = nlohmann::json::parse(serilizedString);
+    p1.name = j1["name"].get<std::string>();
+    p1.age = j1["age"].get<int>();
 }
